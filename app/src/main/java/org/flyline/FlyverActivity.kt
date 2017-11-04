c0e83e78cfb9c787492d67aa840d8f50842ce19a
ethanblake4 <ethanblake@gmail.com>
2017-11-04 09:26:39 -0700

    Implement Flyver base and fix errors

--- app/src/main/java/org/flyline/FlyverActivity.kt
+++ app/src/main/java/org/flyline/FlyverActivity.kt
@@ -1,14 +1,44 @@
 package org.flyline
 
 import android.os.Bundle
+import co.flyver.flyvercore.DroneTypes.QuadCopterX
+import co.flyver.flyvercore.MainControllers.MainController
+import co.flyver.flyvercore.MicroControllers.IOIOController
+import ioio.lib.util.IOIOLooper
 import ioio.lib.util.android.IOIOActivity
 
+
+
 class FlyverActivity : IOIOActivity() {
+
+    val drone = QuadCopterX()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flyver)
-
-
     }
+
+    override fun createIOIOLooper(): IOIOLooper {
+        val ic: IOIOController
+
+        ic = IOIOController(drone).addConnectionHooks(object : IOIOController.ConnectionHooks {
+            override fun onConnect(ioioController: IOIOController) {
+
+                MainController.MainControllerBuilder()
+                        .setMicroController(ioioController)
+                        .setDrone(drone)
+                        .setActivity(this@FlyverActivity)
+                        .build()
+
+                MainController.getInstance().onIoioConnect()
+            }
+
+            override fun onDisconnect() {
+                MainController.getInstance().onIoioDisconnect()
+            }
+        })
+        return ic
+    }
+
+
 }
