e8af2278ff5ca431927c2dbc187f04dce13fcd8c
ethanblake4 <ethanblake@gmail.com>
2017-11-04 12:54:07 -0700

    Add camera and location

---
+++ app/src/main/java/org/flyline/FlyLineApplication.kt
@@ -0,0 +1,16 @@
+package org.flyline
+
+import android.app.Application
+import timber.log.Timber
+
+/**
+ * Sets up initial stuff
+ */
+class FlyLineApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
--- app/src/main/java/org/flyline/FlyverActivity.kt
+++ app/src/main/java/org/flyline/FlyverActivity.kt
@@ -1,44 +1,103 @@
 package org.flyline
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import co.flyver.flyvercore.DroneTypes.QuadCopterX
 import co.flyver.flyvercore.MainControllers.MainController
 import co.flyver.flyvercore.MicroControllers.IOIOController
 import ioio.lib.util.IOIOLooper
 import ioio.lib.util.android.IOIOActivity
-
-
+import kotlinx.android.synthetic.main.activity_flyver.*
 
 class FlyverActivity : IOIOActivity() {
 
-    val drone = QuadCopterX()
+    private val drone = QuadCopterX()
+    private lateinit var cameraController: CameraController
+    private lateinit var locationController: LocationController
+
+    private var initialized = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flyver)
+
+        val cameraPermission = checkSelfPermission(Manifest.permission.CAMERA)
+        val locationPermission = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if(cameraPermission != PackageManager.PERMISSION_GRANTED ||
+                locationPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(mutableListOf<String>().apply {
+                if (cameraPermission != PackageManager.PERMISSION_GRANTED)
+                    add(Manifest.permission.CAMERA)
+                if (locationPermission != PackageManager.PERMISSION_GRANTED)
+                    add(Manifest.permission.ACCESS_FINE_LOCATION)
+            }.toTypedArray(), REQUEST_PERMISSION)
+        } else {
+            cameraController = CameraController(this, autofit)
+            locationController = LocationController(this)
+            initialized = true
+        }
     }
 
-    override fun createIOIOLooper(): IOIOLooper {
-        val ic: IOIOController
+    override fun onResume() {
+        super.onResume()
 
-        ic = IOIOController(drone).addConnectionHooks(object : IOIOController.ConnectionHooks {
-            override fun onConnect(ioioController: IOIOController) {
+        if(initialized) {
+            cameraController.resume()
+            locationController.resume()
+        }
+    }
 
-                MainController.MainControllerBuilder()
-                        .setMicroController(ioioController)
-                        .setDrone(drone)
-                        .setActivity(this@FlyverActivity)
-                        .build()
+    override fun onPause() {
+        super.onPause()
+        if(initialized) {
+            cameraController.pause()
+            locationController.pause()
+        }
+    }
 
-                MainController.getInstance().onIoioConnect()
+    /**
+     * After the camera permission has been accepted or granted, we are notified here
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults.size != permissions.size ||
+                    grantResults.all { it != PackageManager.PERMISSION_GRANTED }) {
+                // The app can't work without both permissions, so just end the application
+                finish()
             }
+        } else {
+            cameraController = CameraController(this, autofit)
+            locationController = LocationController(this)
+            initialized = true
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
 
-            override fun onDisconnect() {
-                MainController.getInstance().onIoioDisconnect()
-            }
+    override fun createIOIOLooper(): IOIOLooper = IOIOController(drone)
+            .addConnectionHooks(object : IOIOController.ConnectionHooks {
+                    override fun onConnect(ioioController: IOIOController) {
+
+                        MainController.MainControllerBuilder()
+                                .setMicroController(ioioController)
+                                .setDrone(drone)
+                                .setActivity(this@FlyverActivity)
+                                .build()
+
+                        MainController.getInstance().onIoioConnect()
+                    }
+
+                    override fun onDisconnect() {
+                        MainController.getInstance().onIoioDisconnect()
+                    }
         })
-        return ic
+
+    companion object {
+        val REQUEST_PERMISSION = 1 /* Identifies camera permission request */
     }
+
 
 
 }
